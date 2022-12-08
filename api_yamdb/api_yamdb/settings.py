f5e5a9328ecc8c7a3d4881e2ef05589176155a42
Олег Биссалиев <bissaliev21@gmail.com>
2022-12-08 20:16:13 +0200

    first_review_2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default='default')
 
 DEBUG = False
 
