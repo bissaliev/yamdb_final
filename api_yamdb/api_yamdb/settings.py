edc061c0a97ef910d5eb29a22680c815584dff9d
bissaliev <bissaliev21@gmail.com>
2024-02-15 17:58:19 +0400

    редактирование докеров

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -112,8 +112,8 @@ AUTH_USER_MODEL = "users.User"
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (os.path.join(BASE_DIR, "static/"),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- api_yamdb/reviews/management/commands/load_csv.py
+++ api_yamdb/reviews/management/commands/load_csv.py
@@ -7,7 +7,6 @@ from django.contrib.auth import get_user_model
 from django.conf import settings
 
 User = get_user_model()
-# User = settings.AUTH_USER_MODEL
 
 from reviews.models import Genre, Category, Title, Review, GenreTitle, Comment
 
@@ -16,106 +15,36 @@ BASE_DIR = settings.BASE_DIR
 
 class Command(BaseCommand):
     help = "Load data from csv file to model"
+    PATH_FILE = os.path.join(BASE_DIR, "data")
+    models = {
+        "genre.csv": Genre,
+        "category.csv": Category,
+        "users.csv": User,
+        "titles.csv": Title,
+        "genre_title.csv": GenreTitle,
+        "review.csv": Review,
+        "comments.csv": Comment,
+    }
 
-    def load_genres(self):
-        file = os.path.join(BASE_DIR, "static", "data", "genre.csv")
-        with open(file, encoding="utf-8") as r_file:
-            reader = csv.DictReader(r_file, delimiter=",")
-            Genre.objects.all().delete()
+    def load_file(self, file, model):
+        with open(
+            f'{os.path.join(BASE_DIR, "data", file)}', encoding='utf-8'
+        ) as f:
+            reader = csv.DictReader(f, delimiter=',')
+            model.objects.all().delete()
             for row in reader:
-                Genre.objects.get_or_create(**row)
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Жанр {row['name']} добавлен в модель Genre."
-                    )
-                )
-            self.stdout.write(self.style.SUCCESS("Записи добавлены."))
+                model.objects.get_or_create(**row)
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Данные для модель '{model}' загружена в БД.")
+            )
 
-    def load_categories(self):
-        file = os.path.join(BASE_DIR, "static", "data", "category.csv")
-        with open(file, encoding="utf-8") as r_file:
-            reader = csv.DictReader(r_file, delimiter=",")
-            Category.objects.all().delete()
-            for row in reader:
-                Category.objects.get_or_create(**row)
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Категория {row['name']} добавлен в модель Category."
-                    )
-                )
-            self.stdout.write(self.style.SUCCESS("Записи добавлены."))
-
-    def load_users(self):
-        file = os.path.join(BASE_DIR, "static", "data", "users.csv")
-        with open(file, encoding="utf-8") as r_file:
-            reader = csv.DictReader(r_file, delimiter=",")
-            # Genre.objects.all().delete()
-            for row in reader:
-                User.objects.get_or_create(**row)
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"user {row['username']} добавлен в модель User."
-                    )
-                )
-            self.stdout.write(self.style.SUCCESS("Записи добавлены."))
-
-    def load_titles(self):
-        file = os.path.join(BASE_DIR, "static", "data", "titles.csv")
-        with open(file, encoding="utf-8") as r_file:
-            reader = csv.DictReader(r_file, delimiter=",")
-            Title.objects.all().delete()
-            for row in reader:
-                Title.objects.get_or_create(**row)
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Произведение {row['name']} добавлен в модель Title."
-                    )
-                )
-            self.stdout.write(self.style.SUCCESS("Записи добавлены."))
-
-    def load_genre_title(self):
-        file = os.path.join(BASE_DIR, "static", "data", "genre_title.csv")
-        with open(file, encoding="utf-8") as r_file:
-            reader = csv.DictReader(r_file, delimiter=",")
-            GenreTitle.objects.all().delete()
-            for row in reader:
-                GenreTitle.objects.get_or_create(**row)
-                self.stdout.write(
-                    self.style.SUCCESS(f"Связь добавлена в модель GenreTitle.")
-                )
-            self.stdout.write(self.style.SUCCESS("Записи добавлены."))
-
-    def load_reviews(self):
-        file = os.path.join(BASE_DIR, "static", "data", "review.csv")
-        with open(file, encoding="utf-8") as r_file:
-            reader = csv.DictReader(r_file, delimiter=",")
-            Review.objects.all().delete()
-            for row in reader:
-                Review.objects.get_or_create(**row)
-                self.stdout.write(
-                    self.style.SUCCESS(f"Отзыв добавлен в модель Review.")
-                )
-            self.stdout.write(self.style.SUCCESS("Записи добавлены."))
-
-    def load_comments(self):
-        file = os.path.join(BASE_DIR, "static", "data", "comments.csv")
-        with open(file, encoding="utf-8") as r_file:
-            reader = csv.DictReader(r_file, delimiter=",")
-            Comment.objects.all().delete()
-            for row in reader:
-                Comment.objects.get_or_create(**row)
-                self.stdout.write(
-                    self.style.SUCCESS(
-                        f"Комментарий добавлен в модель Comment."
-                    )
-                )
-            self.stdout.write(self.style.SUCCESS("Записи добавлены."))
+    def main(self):
+        for csv_file, model in self.models.items():
+            self.load_file(csv_file, model)
+        self.stdout.write(
+            self.style.SUCCESS("Данные полностью загружены в БД.")
+        )
 
     def handle(self, *args: Any, **options: Any):
-        self.load_users()
-        self.load_genres()
-        self.load_categories()
-        self.load_titles()
-        self.load_reviews()
-        self.load_genre_title()
-        self.load_comments()
+        self.main()
