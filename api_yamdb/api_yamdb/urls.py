16d0e68f34bb375766800348e66701383e263d11
bissaliev <bissaliev21@gmail.com>
2024-12-14 08:29:29 +0400

    Редактирование redoc.yaml

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,4 +1,19 @@
+from django.conf import settings
+from django.contrib import admin
+from django.urls import include, path
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path("api/", include("api.urls", namespace="api")),
+    path("admin/", admin.site.urls),
+    path(
+        "redoc/",
+        TemplateView.as_view(template_name="redoc.html"),
+        name="redoc",
+    ),
+]
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns += (path("__debug__/", include(debug_toolbar.urls)),)
